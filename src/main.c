7ce6b10d54624677a2d593c804192f5f8f17cb4c
John Doe <muzz@bk.ru>
2019-10-13 21:21:47 +0300

    changed code and config files a bit

--- src/main.c
+++ src/main.c
@@ -3,20 +3,24 @@
 int main(void) {
   uint8_t it = 0;
 
-  init_sys();
-  init_gpio();
-  init_usart();
+  init_sys();   /* Set MCU speed, enable preipherials, etc.       */
+  init_gpio();  /* Configure ports and pins (PA11 & PC13)         */
+  init_usart(); /* Configure USART6 for 115200 baud               */
+
+  /********             Main program loop                **********/
 
   for (;;) {
-    for (int i = 6; i; i--) {
-      GPIOC->ODR ^= GPIO_ODR_OD13;
-      DELAY_MS(30);
+    for (int i = 6; i; i--) { /* Make 3 fast LED flashes          */
+      GPIOC->ODR ^= GPIO_ODR_OD13; /* invert LED pin state        */
+      DELAY_MS(30); /* Wait 30ms between LED state changings      */
     }
 
-    DELAY_MS(820);
+    DELAY_MS(820);  /* Make LED off for the rest of a second      */
 
-    uprintf("Itteration #%u\n", it++);
+    uprintf("Iteration #%u\n", it++);  /* print debug message     */
   }
+
+/**************        Supress debug message         **************/
 
 #if __CC_ARM
   #pragma diag_suppress 111
