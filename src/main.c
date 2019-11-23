79430abb9df255556a45e50b244690080f3e8b22
John Doe <muzz@bk.ru>
2019-11-24 01:20:07 +0300

    added RTC handling code

--- src/main.c
+++ src/main.c
@@ -1,28 +1,39 @@
 #include "main.h"
 
+__STATIC_INLINE void print_date(void) {
+   uint32_t tr = RTC->TR, dr = RTC->DR;
+
+   u_putx(RTC_DAY(dr)); u_putc('.'); u_putx(RTC_MONTH(dr)); u_putc('.'); u_putc('2'); u_putc('0'); u_putx(RTC_YEAR(dr)); u_putc(' ');
+   u_putx(RTC_HOUR(tr)); u_putc(':'); u_putx(RTC_MINUTE(tr)); u_putc(':'); u_putx(RTC_SECOND(tr)); u_putc(10);
+ }
+
 int main(void) {
 
-  static int it;
-
   init_sys();         /* Set MCU speed, enable preipherials, etc. */
-  init_gpio();        /* Configure ports and pins (PA11 & PC13)   */
+  init_gpio();        /* Configure ports and pins                 */
   init_usart(115200); /* Configure USART6 for 115200 baud         */
-
-  /* ***********           Main program loop         ***********  */
-
+                                                                  /*
+                      Main program loop
+       =================================================           
+                                                                  */
   for (;;) {
+
+    while(!(RTC->ISR & RTC_ISR_WUTF)) {/* JUST WAIT */}
+    PWR->CR = PWR_CR_VOS_1 | PWR_CR_DBP; /* Enable Backup Domain Access (leave VOS default)       */
+    RTC->ISR = ~RTC_ISR_WUTF;
+    PWR->CR = PWR_CR_VOS_1;              /* Disable Backup Domain Access (leave VOS default)      */
+    
     for (int i = 6; i; i--) { /* Make 3 fast LED flashes          */
       GPIOC->ODR ^= GPIO_ODR_OD13; /* invert LED pin state        */
       DELAY_MS(30); /* Wait 30ms between LED state changings      */
     }
 
-    DELAY_MS(820);  /* Make LED off for the rest of a second      */
+    print_date();   /* print debug message                        */
 
-    uprintf("Iteration #%u\n", it++);  /* print debug message     */
-  }
-
-  /*  ***********        Supress debug message       ***********  */
-  
+  }                                                               /*
+                     Supress debug message
+       =================================================           
+                                                                  */
   #if defined(__clang__) && !defined(__CC_ARM)
     #pragma clang diagnostic push
     #pragma clang diagnostic ignored "-Wunreachable-code-return"
